60a9c364329d19cb06ff47ec7e77136da1e2a127
Ranjeet Kumar <ranjeetkumarkanaily@gmail.com>
2016-06-21 03:35:10 +0530

    Remove credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,23 +42,4 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   
-  # config.action_mailer.smtp_settings = {
-  #   :address   => "smtp.mandrillapp.com",
-  #   :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
-  #   :enable_starttls_auto => true, # detects and uses STARTTLS
-  #   :user_name => "Student",
-  #   :password  => "hLXBNafxJOs40GP2ep_nSQ", # SMTP password is any valid API key
-  #   :authentication => 'login', # Mandrill supports 'plain' or 'login'
-  #   :domain => 'xerox.com', # your domain to identify your server when connecting
-  # }
-
-  config.action_mailer.smtp_settings = {
-    :address   => "ssrs.reachmail.net",
-    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
-    :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => "STUDENT12\ranjeet",
-    :password  => "dOb@1309", # SMTP password is any valid API key
-    :authentication => 'plain' # Mandrill supports 'plain' or 'login'
-    # :domain => 'xerox.com', # your domain to identify your server when connecting
-  }
 end
